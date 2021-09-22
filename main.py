2b3e6b8da1e7d47f498b4a667a5a7d3f605ac1e0
A Poor Cat <82797313+poor-cat@users.noreply.github.com>
2021-09-22 08:59:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -149,6 +149,7 @@ print("Validation Log Loss: {}".format(log_loss(y_valid, preds)))
 
 
 
+
 test_files = [im for im in os.listdir(TEST_DIR)]
 test = np.ndarray((len(test_files), ROWS, COLS, CHANNELS), dtype=np.uint8)
 
